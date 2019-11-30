f1b2484becc4517840c525b8a6c9e58e52dd0ac8
minhajsid <minhajsiddiqui69@gmail.com>
2019-11-30 21:41:04 +0500

    .

--- App.js
+++ App.js
@@ -99,16 +99,16 @@ const DrawerNavigatorpanel2 = createDrawerNavigator(
 
 const SwitchNavigator = createSwitchNavigator(
   {
-    splash: Splash,
-    login_register: StackNavigator,
+  //  splash: Splash,
+  //  login_register: StackNavigator,
     passenger: DrawerNavigatorpanel1,
     driver: DrawerNavigatorpanel2,
-    selectride: SelectRide,
-    confirmride: ConfirmRide,
+  //  selectride: SelectRide,
+  //  confirmride: ConfirmRide,
 
   },
   {
-    initialRouteName: 'splash',
+    initialRouteName: 'passenger',
   }
 ); 
 
